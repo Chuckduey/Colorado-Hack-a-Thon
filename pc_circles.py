e583198f02cf91144f27ac022d46d14b517af8e3
Chuck Duey <cduey@msn.com>
2020-02-15 13:11:57 -0700

    New Logo version of the circle

--- pc_circles.py
+++ pc_circles.py
@@ -5,13 +5,13 @@ import tkinter as tk
 screen_height = 400
 screen_width = 400
 center_x = screen_width/2
-center_y = screen_height/2
+center_y = screen_height/2+100
 sin_30 = 100
 cos_30 = 173
 rad=5
 # Define Tk root
 root = tk.Tk()
-canvas = tk.Canvas(root, width=screen_width, height=screen_height, borderwidth=0, highlightthickness=0, bg="#27272d")
+canvas = tk.Canvas(root, width=screen_width, height=screen_height+100, borderwidth=0, highlightthickness=0, bg="#27272d")
 canvas.grid()
 def _create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
@@ -24,16 +24,17 @@ canvas.create_circle(center_x, center_y, 197, fill="#2f2f34", outline="#7d7d81",
 for r in range(175,0,-25):
     canvas.create_circle(center_x, center_y, r, fill="#2f2f34", outline="#7d7d81")
 canvas.create_line(0,center_y,screen_width,center_y,fill="#7d7d81")
-canvas.create_line(center_x,0,center_x,screen_height,fill="#7d7d81")
+canvas.create_line(center_x,100,center_x,screen_height+100,fill="#7d7d81")
 canvas.create_line(center_x-cos_30,center_y-sin_30,center_x+cos_30,center_y+sin_30,fill="#7d7d81")
 canvas.create_line(center_x-sin_30,center_y-cos_30,center_x+sin_30,center_y+cos_30,fill="#7d7d81")
 canvas.create_line(center_x-cos_30,center_y+sin_30,center_x+cos_30,center_y-sin_30,fill="#7d7d81")
 canvas.create_line(center_x-sin_30,center_y+cos_30,center_x+sin_30,center_y-cos_30,fill="#7d7d81")
 root.wm_title("Polar Plot")
 circ1=drawcircle(canvas,100,100,rad)
-mag_block=canvas.create_text(15,10,fill="white",font="Times 15",text="Mag = 1.2", anchor="w")
-ph_block=canvas.create_text(282,10,fill="white",font="Times 15",text="Phase = 180.0", anchor="w")
-
+mag_block=canvas.create_text(15,110,fill="white",font="Times 15",text="Mag = 1.2", anchor="w")
+ph_block=canvas.create_text(282,110,fill="white",font="Times 15",text="Phase = 180.0", anchor="w")
+logo = tk.PhotoImage(file='Keysight_Logo.PNG')
+canvas.create_image(30,5,image=logo, anchor="nw")
 # Start main loop
 for m in range (10,200,10):
   for d in range (361):
