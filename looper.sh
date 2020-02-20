c639cee1f992c219926cb04f2cd68e5b1f36100e
Chuckduey <cduey@msn.com>
2020-02-20 14:26:57 -0700

    fixed up GPIO

--- looper.sh
+++ looper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#gpio mode 4 out  
-mraa-gpio set 7 output
+gpio mode 4 out  
+#mraa-gpio set 7 output
 while true; do  
-#    gpio -g write 4 1
-    mraa-gpio set 7 1
+    gpio -g write 4 1
+#    mraa-gpio set 7 1
     sleep 0.5
-#    gpio -g write  4 0
-    mraa-gpio set 7 0
+    gpio -g write  4 0
+#    mraa-gpio set 7 0
     sleep 0.5
 done  
